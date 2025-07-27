c05814357c2611b8efd2d0daad2ee01f89adcd83
Nikkoola22 <165650747+Nikkoola22@users.noreply.github.com>
2025-07-27 14:03:30 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# RH-cfdt
+# RH-cfdt
---
+++ app.js
@@ -0,0 +1,4 @@
+const App = () => <h1>Bonjour, le fichier HTML est bien ouvert avec React sur Mac !</h1>;
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
